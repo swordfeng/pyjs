1bc809d29bc195d1548b54a4f93fa3c70c607c73
Jianfeng Zhang <swordfeng123@gmail.com>
2016-12-07 16:15:12 +0800

    nodeaio: update

--- nodeaio/nodeaio.js
+++ nodeaio/nodeaio.js
@@ -1,8 +1,8 @@
-const py = require('.')
-const nodeaio = py.import('nodeaio')
+const py = require('..')
+const nodeaio = py.import('nodeaio.nodeaio')
 
 function ensure_coroutine(coro) {
-    return new Promise(nodeaio.ensure_coroutine(coro));
+    return new Promise(nodeaio.ensure_coroutine(coro).then);
 }
 
 module.exports = {
--- nodeaio/nodeaio.py
+++ nodeaio/nodeaio.py
@@ -5,9 +5,12 @@ class LoopThread(threading.Thread):
     def __init__(self):
         super(LoopThread, self).__init__()
         self.loop = asyncio.new_event_loop()
+        self.finalizing = False
     def run(self):
         asyncio.set_event_loop(self.loop)
         self.loop.run_forever()
+        pending = asyncio.Task.all_tasks()
+        self.loop.run_until_complete(asyncio.gather(*pending))
     def stop(self):
         self.loop.call_soon_threadsafe(self.loop.stop)
 
